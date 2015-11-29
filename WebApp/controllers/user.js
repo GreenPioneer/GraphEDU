96a7544871e083a4eb1970c12ccb60d0260e9e47
Thomas Johns <thomasqjohns1@gmail.com>
2015-11-29 17:21:59 -0600

    added group
    added group to user function

--- WebApp/controllers/user.js
+++ WebApp/controllers/user.js
@@ -124,6 +124,7 @@ exports.postUpdateProfile = function(req, res, next) {
     user.profile.gender = req.body.gender || '';
     user.profile.location = req.body.location || '';
     user.profile.website = req.body.website || '';
+    user.profile.group = req.body.group || '';
 
     user.save(function(err) {
       if (err) return next(err);
