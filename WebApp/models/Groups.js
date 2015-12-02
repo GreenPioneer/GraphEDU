5d93258b48593aee0011258a9d636163724ce813
Thomas Johns <thomasqjohns1@gmail.com>
2015-12-02 12:03:16 -0600

    Edited groups schema
    took out the stuff from the user schema

--- WebApp/models/Groups.js
+++ WebApp/models/Groups.js
@@ -4,29 +4,12 @@ var mongoose = require('mongoose');
 
 var groupSchema = new mongoose.Schema({
   //amount of funds left, maximum bank, members, check if they are members
-  funds: { type: String, unique: true, lowercase: true },
-  password: String,
+  funds_remaining: Number,
+  funds_max: Number,
+  members: Array,
+  groupType: String,
+  maxMembers: Number
 
-  facebook: String,
-  twitter: String,
-  google: String,
-  github: String,
-  instagram: String,
-  linkedin: String,
-  tokens: Array,
-
-  profile: {
-    name: { type: String, default: '' },
-    gender: { type: String, default: '' },
-    location: { type: String, default: '' },
-    website: { type: String, default: '' },
-    picture: { type: String, default: '' },
-    group: { type: String, default: '' },
-    paypal: { type: String, default:''}
-  },
-
-  resetPasswordToken: String,
-  resetPasswordExpires: Date
 });
 
-
+module.exports = mongoose.model('Group', groupSchema);
